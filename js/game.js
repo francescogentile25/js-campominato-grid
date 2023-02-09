fb947a8974ef751239fcd539032677f3d0535958
francescogentile25 <francescogentile250893@gmail.com>
2023-02-09 18:36:59 +0100

    FEAT added click event

--- js/game.js
+++ js/game.js
@@ -1,12 +1,22 @@
 console.log('ciao')
-const latoGriglia = 10
+const latoGriglia = prompt('8 liv facile 10 liv difficile')
 const griglia = (latoGriglia * latoGriglia)
 console.log(griglia)
 const box= document.querySelector('.box')
+let celleEl = document.querySelectorAll('.cella')
+console.log(celleEl)
+let newDiv
+//creo struttura griglia 
 for (i=0; i<griglia; i++){
+    const numero = i+1 
     console.log('numero')
     const newDiv = document.createElement("div");
-    newDiv.innerHTML = "Questo è un nuovo div.";
+    newDiv.classList.add("cella");
+    newDiv.innerHTML = numero;
     box.appendChild(newDiv);
+
+    newDiv.addEventListener('click', function () {
+		console.log('ciao')
+	})
 }
 
